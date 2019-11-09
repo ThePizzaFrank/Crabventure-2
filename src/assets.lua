757c736f16ea2198328de1649449f82d8add1dbc
Ethan <ejfweaver@gmail.com>
2019-11-09 17:58:47 -0500

    reorganized asset structure

--- src/assets.lua
+++ src/assets.lua
@@ -1,6 +1,6 @@
 module(...,package.seeall)
 
 images = {
-  enemy = love.graphics.newImage("assets/enemy.png"),
-  wall_1 = love.graphics.newImage("assets/stoneWall.png")
+  enemy = love.graphics.newImage("assets/sprites/enemy.png"),
+  wall_1 = love.graphics.newImage("assets/sprites/stoneWall.png")
 }
