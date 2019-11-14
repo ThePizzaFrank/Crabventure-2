7bcedb0d5605d86d14d5b20d629ee78bcf980b91
Ethan Weaver <terffinatorz@gmail.com>
2019-11-14 11:16:44 -0500

    more filename consistency

--- src/assets.lua
+++ src/assets.lua
@@ -3,12 +3,7 @@ module(...,package.seeall)
 defaultPath = "assets/sprites/"
 
 images = {
-<<<<<<< HEAD
   enemy = love.graphics.newImage("assets/sprites/enemy.png"),
-  wall_1 = love.graphics.newImage("assets/sprites/stoneWall.png"),
-  floor_1 = love.graphics.newImage("assets/sprites/stoneGround.png")
-=======
-  enemy = love.graphics.newImage(defaultPath .. "/enemy.png"),
-  wall_1 = love.graphics.newImage(defaultPath.. "/wall_1.png")
->>>>>>> 8ecb81f790f990fe2a2b9a448996cf8787ef6dcc
+  wall_1 = love.graphics.newImage("assets/sprites/wall_1.png"),
+  floor_1 = love.graphics.newImage("assets/sprites/floor_1.png")
 }
