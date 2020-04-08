d55a68a8b35056fbc18896906d1bb321800d9211
Ethan <ejfweaver@gmail.com>
2020-04-08 14:31:20 -0400

    Update assets.lua

--- src/assets.lua
+++ src/assets.lua
@@ -3,8 +3,8 @@ module(...,package.seeall)
 defaultPath = "assets/sprites/"
 
 images = {
-  enemy = love.graphics.newImage("assets/sprites/enemy.png"),
-  wall_1 = love.graphics.newImage("assets/sprites/wall_1.png"),
-  wall_2 = love.graphics.newImage("assets/sprites/wall_2.png"),
-  floor_1 = love.graphics.newImage("assets/sprites/floor_1.png")
+  enemy = love.graphics.newImage(defaultPath .. "enemy.png"),
+  wall_1 = love.graphics.newImage(defaultPath .. "wall_1.png"),
+  wall_2 = love.graphics.newImage(defaultPath .. "wall_2.png"),
+  floor_1 = love.graphics.newImage(defaultPath .. "floor_1.png")
 }
